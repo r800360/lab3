afe8eded05759c1e35055007bddae17eccf4691c
Rohan Sachdeva <rosachdeva@ucsd.edu>
2024-01-31 10:59:30 -0800

    Updated ArrayExamples and ArrayTests for better implementation

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,24 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testOddReverse() {
+    int[] input1 = { 9,5,8,2,23 };
+    assertArrayEquals(new int[]{ 23, 2, 8, 5, 9 }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testEvenReverse() {
+    int[] input1 = { 9,5,8,2 };
+    assertArrayEquals(new int[]{ 2, 8, 5, 9 }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testEvenReverseInPlace() {
+    int[] input1 = { 9,5,8,5 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 5,8,5,9 }, input1);
+  }
+
 }
